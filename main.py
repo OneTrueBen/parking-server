d737404c3a0fe8734a1b66b242efae22b63efd56
Phillip <phillipmay@videotron.ca>
2019-01-27 03:35:15 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask import request
 from PIL import Image
 import io
 import os
+import datetime
+import json
 
 # Imports the Google Cloud client library
 from google.cloud import vision
@@ -11,6 +13,14 @@ from google.cloud.vision import types
 app = Flask(__name__)
 
 #add conversion to png regardless of image type
+#Some globals for storing flags
+#0 means not relevant
+#1 means no parking
+#2 means only during set time
+IsASign = 0
+CurrentTime = 0
+CurrentDT = 0
+
 
 @app.route('/', methods=['POST'])
 def shit():
@@ -22,16 +32,46 @@ def shit():
 	b = bytearray(content)	
 
 	image = types.Image(content=content)
-	response = client.label_detection(image=image)
-	labels = response.label_annotations
-
-	print('Labels:')
-	for label in labels:
-		print(label.description)
+	response = client.text_detection(image=image)
+	texts = response.text_annotations
 	
+	#contains list of text elements
+	print('Texts:')
+	for text in texts:
+		print('\n"{}"'.format(text.description))
+	
+	#Get logo tags to make sure it's an image
+	response = client.logo_detection(image=image)
+	logos = response.logo_annotations
+	print('Logos:')
+	
+	for logo in logos:
+		if (logo.description == "bad religion"):
+			IsASign = 1
+		print(logo.description)	
+	#Actual Guessing of signs meaning
+	currentDT = datetime.datetime.now()
+	print('Is a sign')
+	print(IsASign)
+	#Extract conditions start time
+	FirstTime = texts[0].description.find('h')
+	print('Start time')
+	print(texts[0].description[(FirstTime-2):(FirstTime)])
+	#Extract conditions end time
+	#Code for no parking
+	#SecondTime = texts[0].description[(FirstTime):].description.find('h')
+	#print('End time')
+	#print(texts[0].description[(SecondTime-2):(SecondTime)])
+	
+	
+	#Json stuff
+	data = {}
+	data['Title'] = 'Image Result'
+	data['Text'] = 'The main windows text'
+	json_data = json.dumps(data)	
+	#end of json stuff
 	print('Something')
-	#img = Image.open(request.data)
-	return 'Done'
+	return json_data
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=80)
